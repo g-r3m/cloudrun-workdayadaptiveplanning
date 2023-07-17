78f4b12eed3a63f514d08a4272ed91bcb2d1c805
Jeremy <35274967+g-r3m@users.noreply.github.com>
2023-07-17 22:21:46 +0200

    Update main.py
    Modify main.py

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ It is responsible for:
 - Parsing the XML
 - Transforming the data
 - Loading the data into BigQuery
+More info on the API(https://doc.workday.com/adaptive-planning/en-us/integration/managing-data-integration/api-documentation/metadata-and-data-create-update-and-read-methods/mdn1623709213322.html)
 """
+# test
 import os
+import csv
 import requests
 import xml.etree.ElementTree as et
 import json
 import pandas as pd
+import re
 from datetime import datetime
 from google.cloud import bigquery
 from google.cloud import secretmanager
@@ -37,28 +41,22 @@ def main():
     """
     Main function
     """
-    start = request.args.get('START_DATE') or os.environ.get(
-        'START_DATE', 'DD/YYYY')
-    period = start.replace('/', '')
-    account = request.args.get(
-        'ACCOUNT') or os.environ.get('ACCOUNT', 'Account1')
-    level_name = request.args.get(
-        'LEVEL_NAME') or os.environ.get('LEVEL_NAME', 'LevelName1')
-    dim_name = request.args.get(
-        'DIM_NAME') or os.environ.get('DIM_NAME', 'DimName1')
-    dimname = dim_name.replace('/', '')        
-    dim = request.args.get('DIM') or os.environ.get('DIM', 'Dim1')
-    version_name = request.args.get('VERSION_NAME') or os.environ.get(
-        'VERSION_NAME', 'VersionName1')
+    current_year = datetime.now().year
+    start = request.args.get('START_DATE') or os.environ.get('START_DATE', f'01/{current_year}')
+    end = request.args.get('END_DATE') or os.environ.get('END_DATE', f'12/{current_year}')
+    level_name = request.args.get('LEVEL_NAME') or os.environ.get('LEVEL_NAME', 'H500A')
+    dim_name = request.args.get('DIM_NAME') or os.environ.get('DIM_NAME', 'FF/FI')
+    dim = request.args.get('DIM') or os.environ.get('DIM', 'FI')
+    version_name = request.args.get('VERSION_NAME') or os.environ.get('VERSION_NAME', 'REEL_AGORA')
     # Set the Google Cloud Platform project, dataset, and table details
-    project_id = "PROJECT_ID"
-    dataset_id = "DATASET_ID"
-    table_id = "TABLE_ID"
+    project_id = "$PROJECT_ID"
+    dataset_id = "*DATASET_ID"
+    table_id = "$TABLE_ID"
     # Set & Get the WAP API
     url = "https://api.adaptiveinsights.com/api/v36"
-    data = get_secret(project_id, "ADAPTIVE_LOGIN")
-    login = data['login']
-    password = data['password']
+    data = get_secret(project_id, "adaptive_login")
+    login = data["login"]
+    password = data["password"]
     payload = f"""<?xml version='1.0' encoding='UTF-8'?>
     <call method="exportData" callerName="wap-extract">
     <credentials login="{login}" password="{password}"/>
@@ -66,15 +64,19 @@ def main():
     <format useInternalCodes="true" includeUnmappedItems="false" />
     <filters>
     <accounts>
-        <account code="{account}" isAssumption="false" includeDescendants="true"/>
+      <account code="ChargesEbitda" isAssumption="false" includeDescendants="true"/>
+      <account code="CoutsNonCourant" isAssumption="false" includeDescendants="true"/>
+      <account code="ChargesCourantesNonCash" isAssumption="false" includeDescendants="true"/>
+      <account code="ResultatHorsExploitation" isAssumption="false" includeDescendants="true"/>
+      <account code="ProduitsExploitation" isAssumption="false" includeDescendants="true"/>                     
     </accounts>
     <levels>
-        <level name="{level_name}" isRollup="true" includeDescendants="false"/>
+        <level name="{level_name}" isRollup="true" includeDescendants="true"/>
     </levels>
     <dimensionValues>
         <dimensionValue dimName="{dim_name}" name="{dim}" directChildren="false"/>
     </dimensionValues>
-    <timeSpan start="{start}" end="{start}"/>
+    <timeSpan start="{start}" end="{end}"/>
     </filters>
     <dimensions>
     <dimension name="{dim_name}"/>
@@ -86,104 +88,84 @@ def main():
     headers = {
         'Content-Type': 'application/xml'
     }
-    print(payload)
     response = requests.request("POST", url, headers=headers, data=payload)
-    print(response.text)
     # Save the result in a file in the /tmp folder
     file_path = "/tmp/output.xml"
-
+    #debug var bellow
     with open(file_path, "wb") as file:
         for chunk in response.iter_content(chunk_size=128):
             file.write(chunk)
     # Parse the XML
     xtree = et.parse(file_path)
     root = xtree.getroot()
-    print(xtree)
     # Extract the output text from CDATA
-    output_text = root.find("./output")
-    if output_text is not None:
-        output_text = output_text.text
-        # Split the output into lines
-        lines = output_text.strip().split('\n')
-        # Split the header and values
-        header = lines[0].split(',')
-        values = lines[1].split(',')
-        # Create a dictionary to store the data
-        data_dict = {}
-        for i, col in enumerate(header):
-            data_dict[col] = [values[i]]
-        # Create the DataFrame
-        df = pd.DataFrame(data_dict)
-        # Remove specific characters from the table name
-        # Remove specific characters from dtype names
-        df.columns = df.columns.str.replace('%', '')
-        df.columns = df.columns.str.replace('/', '')
-        df.columns = df.columns.str.replace('!', '')
-        df.columns = df.columns.str.replace('"', '')
-        # Preprocess DataFrame to remove extra quotes
-        df["Account Name"] = df["Account Name"].str.strip('"""')
-        df["Account Code"] = df["Account Code"].str.strip('"""')
-        df["Level Name"] = df["Level Name"].str.strip('"""')
-        df[dimname] = df[dimname].str.strip('"""')
-        df = df.rename(columns={
-            "Account Name": "Name",
-            "Account Code": "Code",
-            "Level Name": "Lvl_Name",
-            dimname: "Dim_name",
-            period: "Value"
-        })
-        # Add related info (from columns in WAP APIs)
-        df["Period"] = period
-        df["Dim"]= dim
-        # Get current timestamp
-        current_timestamp = datetime.now()
-        # Convert timestamp to desired format
-        current_timestamp = pd.to_datetime(
-            current_timestamp).strftime("%Y-%m-%d %H:%M:%S")
-        # Add insert_timestamp column to DataFrame
+    data = root.find('output')
+    if data is not None:
+        # Get the data from the XML
+        data = root.find('output').text.strip().split('\n')
+        header = data[0].replace(' ', '').replace('"', '').replace('/', '')
+        rows = data[1:]
+        # Clean the header
+        # Create a CSV file
+        # Put file in /tmp/ folder for GCR
+        with open('/tmp/output.csv', 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            
+            # Write the header add insertion_timestamp
+            writer.writerow(['insertion_timestamp'] + header.split(','))
 
-        df["insert_timestamp"] = current_timestamp        
-        from google.cloud import bigquery
-
-        # Create a BigQuery client using the default credentials
+            # Write the rows
+            for row in rows:
+                timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                writer.writerow([timestamp] + row.split(','))
+                            
+        # Create a BigQuery client
         client = bigquery.Client(project=project_id)
-        # Print df transformed ready for loading
-        print(df)
-        table_ref = f"{project_id}.{dataset_id}.{table_id}"
+
+        # Set the path to the CSV file
+        csv_file = "/tmp/output.csv"
+        # Define the BigQuery dataset and table references
+        table_ref = client.dataset(dataset_id).table(table_id)
+
+        # Define the job configuration
         job_config = bigquery.LoadJobConfig()
-        job_config.write_disposition = bigquery.WriteDisposition.WRITE_TRUNCATE
-        job_config.schema = [
-            bigquery.SchemaField("Name", bigquery.enums.SqlTypeNames.STRING),
-            bigquery.SchemaField("Code", bigquery.enums.SqlTypeNames.STRING),
-            bigquery.SchemaField(
-                "Lvl_Name", bigquery.enums.SqlTypeNames.STRING),
-            bigquery.SchemaField("Dim_name", bigquery.enums.SqlTypeNames.STRING),
-            bigquery.SchemaField("Dim", bigquery.enums.SqlTypeNames.STRING)
-        ]
-        print(df)
-        # Write DataFrame to BigQuery table
-        client.load_table_from_dataframe(
-            df, table_ref, job_config=job_config).result()
+        job_config.source_format = bigquery.SourceFormat.CSV
+        job_config.write_disposition = bigquery.WriteDisposition.WRITE_APPEND
+        job_config.skip_leading_rows = 1  # Skip the header row
+        job_config.allow_jagged_rows = True  # Allow jagged rows
+        job_config.quote_character = '"'  # Set the quote character to double quote
+
+
+        # Load data from CSV file into BigQuery table
+        with open(csv_file, "rb") as file:
+            job = client.load_table_from_file(file, table_ref, job_config=job_config)
+
+        job.result()  # Wait for the job to complete        
         table = client.get_table(table_ref)  # Make an API request.
         print(
-            "Loaded {} rows and {} columns to {}".format(
-                table.num_rows, len(table.schema), table_id
-            )
-        )
+             "Loaded {} rows and {} columns to {}".format(
+                 table.num_rows, len(table.schema), table_id
+             )
+         )
         # return an appropriate response
         loaded_data = {
-            'success': 'Workday Adaptive Planning response',
-            'response_text': response.text,
+            "success": "Workday Adaptive Planning response",
+            "response_text": response.text,
         }
-    else: 
+    else:
         # return an appropriate error response
         error_data = {
-            'error': 'Workday Adaptive Planning error',
-            'response_text': response.text,
+            "error": "Workday Adaptive Planning error",
+            "response_text": response.text,
         }
         return jsonify(error_data), 500
 
-    return jsonify(loaded_data), 200    
+    return jsonify(loaded_data), 200
+
+
+if __name__ == "__main__":
+    server_port = os.environ.get("PORT", "8080")
+    app.run(debug=False, port=server_port, host="0.0.0.0")
 
 
 if __name__ == "__main__":
